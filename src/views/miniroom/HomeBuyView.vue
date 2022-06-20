d91f0c538b8f73e875b71201180c144051fec8e5
이전영 <ijeon-yeong@ijeon-yeong-ui-MacBookAir.local>
2022-06-20 15:26:45 +0900

    commit

--- src/views/miniroom/HomeBuyView.vue
+++ src/views/miniroom/HomeBuyView.vue
@@ -10,6 +10,7 @@
         <button class="btn btn-warning"><router-link to="/home/buy">아이템구매</router-link></button><br>
         <button class="btn btn-warning"><router-link to="/home/style">아이템적용</router-link></button><br>
         <button class="btn btn-warning"><router-link to="/home/wish">위시리스트</router-link></button><br>
+
       </span>
       </div>
 <div class="table-responsive">
