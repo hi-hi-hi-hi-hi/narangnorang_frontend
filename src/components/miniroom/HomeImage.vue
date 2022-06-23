a68633e4bcb3539a0ededa935eb402987361db5a
이전영 <ijeon-yeong@ijeon-yeong-ui-MacBookAir.local>
2022-06-23 17:27:57 +0900

    fix: 아이템 적용 바로되기, View 파일들 Component로 이동

--- src/components/miniroom/HomeImage.vue
+++ src/components/miniroom/HomeImage.vue
@@ -31,8 +31,16 @@
 import axios from 'axios'
 
 export default {
+  props: {
+    ImgRefres: Object
+  },
   created () {
     this.getImage()
+  },
+  updated () {
+    if (this.ImgRefres === 1) {
+      this.getImage()
+    }
   },
   name: 'HomeView',
   data () {
--- src/views/miniroom/HomeView.vue
+++ src/views/miniroom/HomeView.vue
@@ -1,32 +1,36 @@
 <template>
-
     <div v-if="privilege ===3" class="section">
       <div class="miniroomSideBar">
     <MiniroomSideBar v-on:group="groupp" />
     내 포인트: {{point}}포인트
     </div>
-      <div class="align">
-        <HomeImage />
+    <div class="section-1">
+      <div class="home-img border border-3">
+        <HomeImage :ImgRefres="ImgRefres"/>
+        <MiniroomButton v-on:group="groupp"></MiniroomButton>
       </div>
       <div></div>
       <div v-if="group === 'B'">
         <HomeBuy v-on:point="getImage"></HomeBuy>
       </div>
       <div v-else-if="group === 'S'">
-        <HomeStyle></HomeStyle>
+        <HomeStyle v-on:ImgRefresh="ImgRefresh"></HomeStyle>
       </div>
       <div v-else-if="group === 'W'">
         <HomeWish></HomeWish>
       </div>
    </div>
+    </div>
 </template>
 
 <script>
 import HomeImage from '@/components/miniroom/HomeImage.vue'
 import MiniroomSideBar from '@/components/miniroom/MiniroomSideBar'
-import HomeBuy from '@/views/miniroom/HomeBuyView'
-import HomeStyle from '@/views/miniroom/HomeStyleView'
-import HomeWish from '@/views/miniroom/HomeWishView'
+import HomeBuy from '@/components/miniroom/HomeBuy'
+import HomeStyle from '@/components/miniroom/HomeStyle'
+import HomeWish from '@/components/miniroom/HomeWish'
+import MiniroomButton from '@/components/miniroom/MiniroomButton'
+
 export default {
   name: 'HomeView',
   components: {
@@ -43,7 +47,8 @@ export default {
     return {
       privilege: '',
       point: '',
-      group: ''
+      group: '',
+      ImgRefres: ''
     }
   },
   methods: {
@@ -59,6 +64,9 @@ export default {
     },
     groupp (group) {
       this.group = group
+    },
+    ImgRefresh () {
+      this.ImgRefres = 1
     }
 }
 }
